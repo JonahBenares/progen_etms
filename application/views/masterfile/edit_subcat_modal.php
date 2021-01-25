80751de3b739736008ce2fa5c451afbcdd4b7fe8
unknown <glennpaul.cenpri@gmail.com>
2021-01-25 13:25:13 -0800

    Update location dropdown

--- application/views/masterfile/edit_subcat_modal.php
+++ application/views/masterfile/edit_subcat_modal.php
@@ -6,8 +6,11 @@
         <label for="" class="control-label mb-1">Location:</label>
         <select name="location" class="form-control bor-radius5 cc-cvc">
             <option value = "">--Select Location--</option>
-            <option value = "BCD" <?php echo (($s->location == 'BCD') ? ' selected' : '');?>>Bacolod</option>
-            <option value = "BS" <?php echo (($s->location == 'BS') ? ' selected' : '');?>>Bago</option>
+            <?php foreach($location AS $l){ ?>
+            <option value = "<?php echo $l->location_prefix; ?>" <?php echo (($s->location == $l->location_prefix) ? ' selected' : '');?>><?php echo $l->location_name; ?></option>
+            <?php } ?> 
+            <!-- <option value = "BCD" <?php echo (($s->location == 'BCD') ? ' selected' : '');?>>Bacolod</option>
+            <option value = "BS" <?php echo (($s->location == 'BS') ? ' selected' : '');?>>Bago</option> -->
         </select>
         <label for="" class="control-label mb-1">Prefix Name:</label>
         <input id="" name="prefix" type="text" class="form-control bor-radius5" value = "<?php echo $s->subcat_prefix;?>">
